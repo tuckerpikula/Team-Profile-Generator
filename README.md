a2d79adf72248191c524ea498fd9e22908111c79
pikulatucker@gmail.com <pikulatucker@gmail.com>
2021-03-11 07:23:43 -0800

    Final

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Team-Profile-Generator
-
+<!-- Link to rep -->
 https://github.com/tuckerpikula/Team-Profile-Generator
+
+<!-- Embedded code to video -->
+<iframe src="https://drive.google.com/file/d/1-LRKkiILCKoyqh6TufQXf1rj6i5BF_-B/preview" width="640" height="480"></iframe>
+
+<!-- Link to google drive video -->
+https://drive.google.com/file/d/1-LRKkiILCKoyqh6TufQXf1rj6i5BF_-B/view
+
+<!-- Screenshot of application on webpage -->
+<img src="./employee.png" alt="screenshot of employees and app>
--- app.js
+++ app.js
@@ -10,9 +10,170 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employee = []
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+let employeeFunction = () => {
+  inquirer.prompt([{
+    type: 'list',
+    name: 'Roles',
+    message: 'Select role',
+    choices: ['Manager', 'Engineer', 'Intern']
+  }])
+
+.then(res => {
+  let role = res.Roles
+  if (role === 'Manager') {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Write name here:'
+      }, {
+        type: 'input',
+        name: 'id',
+        message: 'Write ID here:'
+      }, {
+        type: 'input',
+        name: 'email',
+        message: 'Write email here:'
+      }, {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Write office number here:'
+      }
+    ])
+    .then(res => {
+      let name = res.name
+      let id = res.id
+      let email = res.email
+      let officeNumber = res.officeNumber
+      let newManager = new Manager(name, id, email, officeNumber)
+      employee.push(newManager)
+      fs.writeFile(outputPath, render(employee), err => {
+        if (err) {console.log(err)}
+      })
+      inquirer.prompt([{
+        type: 'list',
+        name: 'continue',
+        message: 'Do you wish to add another employee?',
+        choices: ['Yes', 'No']
+      }])
+      .then(res => {
+        if (res.continue === 'Yes') {
+          employeeFunction ()
+        } else {
+          console.log('Finish')
+        }
+      })
+      .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+  }
+  if (role === 'Engineer') {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Write name here:'
+      }, {
+        type: 'input',
+        name: 'id',
+        message: 'Write ID here:'
+      }, {
+        type: 'input',
+        name: 'email',
+        message: 'Write Email here:'
+      }, {
+        type: 'input',
+        name: 'github',
+        message: 'Write GitHub username here:'
+      }
+    ])
+    .then (res => {
+      let name = res.name
+      let id = res.id
+      let email = res.email
+      let github = res.github
+      let newEngineer = new Engineer(name, id, email, github)
+      employee.push(newEngineer)
+      fs.writeFile(outputPath, render(employee), err => {
+        if (err) {console.log(err)}
+      })
+      inquirer.prompt([{
+        type: 'list',
+        name: 'continue',
+        message: 'Do you wish to add another employee?',
+        choices: ['Yes', 'No']
+      }])
+      .then (res => {
+        if (res.continue === 'Yes') {
+          employeeFunction()
+        } else {
+          console.log('Finish')
+        }
+      })
+      .catch(err => console.log(err))
+    })
+    .catch(err => console.logg(err))
+  }
+  if (role === 'Intern') {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Write name here:'
+      }, {
+        type: 'input',
+        name: 'id',
+        message: 'Write ID here:'
+      }, {
+        type: 'input',
+        name: 'email',
+        message: 'Write Email here:'
+      }, {
+        type: 'input',
+        name: 'school',
+        message: 'Write what school you went to here:'
+      }
+    ])
+    .then(res => {
+      let name = res.name
+      let id = res.id
+      let email = res.email
+      let school = res.school
+      let newIntern = new Intern(name, id, email, school)
+      employee.push(newIntern)
+      fs.writeFile(outputPath, render(employee), err => {
+        if (err) {console.log(err)}
+      })
+      inquirer.prompt([{
+        type: 'list',
+        name: 'continue',
+        message: 'Do you wish to add another employee?',
+        choices: ['Yes', 'No']
+      }])
+      .then(res => {
+        if (res.continue === 'Yes') {
+          employeeFunction()
+        } else {
+          console.log('Finish')
+        }
+      })
+      .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+  }
+})
+.catch(err => console.log(err))
+}
+
+employeeFunction()
+
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
